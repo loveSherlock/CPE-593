8addad4fc96a57b7c72c7f1a02c176449602e0c1
loveSherlock <fanxz@me.com>
2016-03-31 09:31:11 -0400

    new project

---
+++ CPE-593-group_project/CPE-593-group_project/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPE-593-group_project
+//
+//  Created by FANXUEZHOU on 16/3/31.
+//  Copyright © 2016年 FANXUEZHOU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
